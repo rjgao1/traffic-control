e193a168efdea48cfb94395a9c78929f324170f1
Tabitha Lee <tabitha.see.ya.lee@vanderbilt.edu>
2022-12-15 14:19:06 -0600

    Add controller

--- TrafficLight.py
+++ TrafficLight.py
@@ -6,18 +6,10 @@ class TrafficLight:
         self.light = Color.R
         self.carsWaiting = 0
 
-        # output variables
-        self.lightOut = None
-        self.carsWaitingOut = None
-
-
     def emit_output(self):
         lightOut = self.light
         carsWaitingOut = self.carsWaiting
         return lightOut, carsWaitingOut
-    
-    # input for TrafficLight = (control, carsArriving)
-    # input for Controller = [(), (), (), ()]
     
     def handle_input(self, input):
         control, carsArriving = input
---
+++ TrafficLightController.py
@@ -0,0 +1,52 @@
+from Color import Color
+
+class TrafficLightController:
+    def __init__(self):
+        self.north = Color.G
+        self.south = Color.G
+        self.east = Color.R
+        self.west = Color.R
+
+        self.clock = 0
+
+    def emit_output(self):
+        controlNorth = self.north
+        controlSouth = self.south
+        controlEast = self.east
+        controlWest = self.west
+        return controlNorth, controlSouth, controlEast, controlWest
+    
+    def update_clock(self):
+        self.clock += 1
+    
+    def handle_input(self, input):
+        northInfo, southInfo, eastInfo, westInfo = input
+
+        lightNorth, carsNorth = northInfo
+        lightSouth, carsSouth = southInfo
+        lightEast, carsEast = eastInfo
+        lightWest, carsWest = westInfo
+
+        if lightNorth == Color.Y or lightSouth == Color.Y:
+            self.north = Color.R
+            self.south = Color.R
+            self.east = Color.G
+            self.west = Color.G
+
+        if lightNorth == Color.Y or lightSouth == Color.Y:
+            self.north = Color.G
+            self.south = Color.G
+            self.east = Color.R
+            self.west = Color.R
+
+        if (carsNorth > 0 or carsSouth > 0) and (self.clock % 3 < 2):
+            if lightEast == Color.G or lightSouth == Color.G:
+                self.east = Color.Y
+                self.west = Color.Y
+        elif (carsEast > 0 or carsWest > 0):
+            if lightNorth == Color.G or lightSouth == Color.G:
+                self.north = Color.Y
+                self.south = Color.Y
+        elif lightEast == Color.G or lightWest == Color.G:
+            self.east = Color.Y
+            self.west = Color.Y
